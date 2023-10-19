ad45f3118db7c90fe6b1a8130c0860383c4869ec
Y4h14 <yahyasadig2013@gmail.com>
2023-10-19 18:40:33 +0300

    cd fix

--- cd.c
+++ cd.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <unistd.h>
-
+#include <stdlib.h>
+#include <linux/limits.h>
+#include <libgen.h>
 /**
  * _cd - change the current directory
  * @path: path to the new directory
@@ -12,18 +14,17 @@ int _cd(char *path)
 {
 	char *home = getenv("HOME");
 	char *oldpwd = getenv("PWD");
-	char *newpwd;
-	int i = 0;
+	char *newpwd, temp[PATH_MAX];
+	int i = 0, s1 = 0, s2 = 0;
 
 	if (path == NULL)
-	{
 		newpwd = home;
-	}
+
 	else if (_strcmp(path, "-") == 0)
 	{
 		if (oldpwd != NULL)
 		{
-			newpwd = oldpwd;
+			newpwd = getenv("OLDPWD");
 			i = 1;
 		}
 		else
@@ -33,17 +34,19 @@ int _cd(char *path)
 		}
 	}
 	else
-	{
-		newpwd = path;
-	}
+		newpwd = realpath(path, temp);
+
 	if (chdir(newpwd) == -1)
 	{
 		perror("chdir");
 		return (1);
 	}
-	setenv("PWD", newpwd, 1);
-	setenv("OLDPWD", oldpwd, 1);
-
+	s1 = setenv("PWD", newpwd, 1);
+	s2 = setenv("OLDPWD", oldpwd, 1);
+	if (s2 == -1)
+		fprintf(stderr, "error setting OLDPWD");
+	if (s1 == -1)
+		fprintf(stderr, "error setting OLDPWD");
 	if (i == 1)
 		print_pwd();
 
